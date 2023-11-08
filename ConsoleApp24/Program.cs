1a5731dda36b34781772b6e39affaae178df3727
gkvnoderr <123897793+gkvnoderr@users.noreply.github.com>
2023-11-08 22:24:34 +0300

    Add files via upload

---
+++ ConsoleApp24/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.1
+            //static void Func(double x = 1,double z = 2, double c = 3)
+            //{
+            //    double y = ((x + Math.Sin(x)) / 3) + ((z + Math.Sin(z)) / 3) + ((c + Math.Sin(c)) / 3);
+            //    Console.Write($"{y:f2}");
+            //}
+            //static void Main(string[] args)
+            //{
+            //    Func(1,5,7);
+            //    Console.ReadKey();
+            //}
+
+            //// 1.2
+            //static void Seconds(int h = 1, int m = 10, int s = 20)
+            //{
+            //    int seconds = h * 60 * 60 + m * 60 + s;
+            //    Console.Write($"Общее количество секунд:{seconds}");
+            //}
+            //static void Main()
+            //{
+            //    Console.Write("Введите часы: ");
+            //    int h = int.Parse(Console.ReadLine());
+            //    Console.Write("Введите минуты: ");
+            //    int m = int.Parse(Console.ReadLine());
+            //    Console.Write("Введите секунды: ");
+            //    int s = int.Parse(Console.ReadLine());
+            //    Seconds(h, m, s);
+            //    Console.ReadKey();
+            }
+    }
+}
